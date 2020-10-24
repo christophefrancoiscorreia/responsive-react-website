c772321eccff09f7290becfefcef135c3d65c9f5
Christophe Correia <cc8design@gmail.com>
2020-10-25 00:40:40 +0200

    qsd

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,7 +35,7 @@ function Navbar() {
                     <div className="navbar-container container">
                         <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
                             <MdFingerprint className="nabar-icon" /> 
-                            LAVISH 
+                            MY BANK
                         </Link>
                         <div className="menu-icon" onClick={handleClick}>
                             {click ? <FaTimes /> : <FaBars /> }
